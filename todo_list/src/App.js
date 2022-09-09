95bfe805957e6500615303dba7f7051ddd63e5db
Hagen <73616486+Hagen-Fulford@users.noreply.github.com>
2022-09-09 15:59:06 +0700

    adding content

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+}
+
 function App() {
   return (
-    <div>
-      
+    <div className ={style.bg}>
+      <div className={style.containter}>
+      <h3 className={style.heading}>Todo App</h3>
+      <form className={style.form}><input classname={style.input} type="text" placeholder='Add Todo' /></form>
+      <button className={style.button}></button>
+
+      </div>
     </div>
   );
 }
